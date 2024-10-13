4b1253198d7701a73789c72053cb1da5853485cd
Arielle <a.lucca@hotmail.com.br>
2024-10-13 09:13:52 -0300

    Logo to webp

--- header.php
+++ header.php
@@ -12,8 +12,8 @@
     <?php if (file_exists(get_template_directory() . '/assets/img/home-bg.webp')) { ?>
         <link rel="preload" as="image" href="<?php echo esc_attr(get_template_directory_uri() . '/assets/img/home-bg.webp') ?>"  />
     <?php } ?>
-    <?php if (file_exists(get_template_directory() . '/assets/img/cool-kids-network-high-resolution-logo-transparent.png')) { ?>
-        <link rel="preload" as="image" href="<?php echo esc_attr(get_template_directory_uri() . '/assets/img/cool-kids-network-high-resolution-logo-transparent.png') ?>"  />
+    <?php if (file_exists(get_template_directory() . '/assets/img/new-logo.webp')) { ?>
+        <link rel="preload" as="image" href="<?php echo esc_attr(get_template_directory_uri() . '/assets/img/new-logo.webp') ?>"  />
     <?php } ?>
 
     <?php wp_head(); ?>
--- includes/navbar.php
+++ includes/navbar.php
@@ -1,8 +1,8 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-  <?php if (file_exists(get_template_directory() . "/assets/img/new-logo.jpg" )) { ?>
+  <?php if (file_exists(get_template_directory() . "/assets/img/new-logo.webp" )) { ?>
       <a class="navbar-brand" href="/">
-        <img src="<?php echo get_template_directory_uri() ?>/assets/img/new-logo.jpg" width="250" height="100" alt="Cool Kids Network Logo" />
+        <img src="<?php echo get_template_directory_uri() ?>/assets/img/new-logo.webp" width="250" height="100" alt="Cool Kids Network Logo" />
       </a>
     <?php } ?>
     <?php if (is_user_logged_in()) { ?>
